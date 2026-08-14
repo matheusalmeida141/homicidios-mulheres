b35051ff32fdc91509cffde44134485b62224ea5
Matheus <matheus.almeida.dev@gmail.com>
2026-08-14 12:39:09 -0300

    fix: Arrumado coleta de dados nos mêses

--- notebooks/coleta.py
+++ notebooks/coleta.py
@@ -1,6 +1,17 @@
-# %%
 import pandas as pd
 import requests
+
+response = requests.get(f"https://www.ssp.sp.gov.br/v1/ViolenciaMulher/RecuperaDadosPorAno?ano=2025")
+
+lst = []
+for k in response.json()['data'][0]['dadosMes']:
+    lst.append(k['delito'])
+
+del k
+
+dfDelitos = pd.DataFrame(lst)
+dfDelitos = dfDelitos.drop(columns=['idGrupoDelito', 'ordem'])
+dfDelitos = dfDelitos.set_index('idDelito')
 
 for i in range(2011, 2026):
     response = requests.get(f"https://www.ssp.sp.gov.br/v1/ViolenciaMulher/RecuperaDadosPorAno?ano={i}")
@@ -12,30 +23,24 @@ for i in range(2011, 2026):
         for j in range(len(response.json()['data'])):
 
             mes = response.json()['data'][j]['mes']
-
             dfaux = pd.DataFrame(response.json()['data'][j]['dadosMes'])
             dfaux = dfaux.drop(columns="delito")
             dfaux['mes'] = mes
 
-            df = pd.concat([df, dfaux])  
-        print(df)
-        # lst = []
-        # for j in response.json()['data'][1]['dadosMes']:
-        #     lst.append(j['delito']) 
-        # df2 = pd.DataFrame(lst)
-        # df2.set_index("idDelito", inplace=True)
-        # df = df.join(df2, on=["idDelito"], how="left")
-        # df.drop(columns = ["idGrupoDelito", "ordem"], inplace= True)
+            df = pd.concat([df, dfaux])
+            del dfaux  
+       
 
+        df = df.join(dfDelitos, on="idDelito", how="left")
+        
 
-        #del df2
+        df["ano"] = i
 
-        #df.to_csv(f"../data/violencia-mulher-{i}.csv", index=True, encoding="utf-8")
-
-        #del df
+        df.to_csv(f"../data/violencia-mulher-{i}.csv", index=False, encoding="utf-8")
+        
+        del df
+        
         print("CSV gerado com sucesso!")
     else:
         print("Requisição 🔴")
 
-
-# %%
--- notebooks/etl.py
+++ notebooks/etl.py
@@ -4,9 +4,10 @@ import pandas as pd
 df = pd.DataFrame()
 for i in range(2011, 2026):
     df2 = pd.read_csv(f"../data/violencia-mulher-{i}.csv",)
-    df2["ano"] = i
 
     df = pd.concat([df, df2])
-# %%
-df[ df["idDelito"] == 134]
+
+
+df
+
 # %%
