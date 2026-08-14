2a60125fa6a046c20998eb692d9867340320af6c
Matheus <matheus.almeida.dev@gmail.com>
2026-08-14 11:59:36 -0300

    Pegado dados por mês de todos os anos, porém se desc ainda

--- notebooks/coleta.py
+++ notebooks/coleta.py
@@ -1,4 +1,4 @@
-
+# %%
 import pandas as pd
 import requests
 
@@ -8,28 +8,34 @@ for i in range(2011, 2026):
     if response.status_code == 200:
         print("Requisição 🟢")
 
-        for j in range(0, 13):
+        df = pd.DataFrame()
+        for j in range(len(response.json()['data'])):
 
-            df = pd.DataFrame(response.json()['data'][j]['dadosMes'])
-            df["mes"] = j
+            mes = response.json()['data'][j]['mes']
 
-            df = df.drop(columns=['delito'])
+            dfaux = pd.DataFrame(response.json()['data'][j]['dadosMes'])
+            dfaux = dfaux.drop(columns="delito")
+            dfaux['mes'] = mes
 
-        lst = []
-        for j in response.json()['data'][1]['dadosMes']:
-            lst.append(j['delito']) 
-        df2 = pd.DataFrame(lst)
-        df2.set_index("idDelito", inplace=True)
-        df = df.join(df2, on=["idDelito"], how="left")
-        df.drop(columns = ["idGrupoDelito", "ordem"], inplace= True)
+            df = pd.concat([df, dfaux])  
+        print(df)
+        # lst = []
+        # for j in response.json()['data'][1]['dadosMes']:
+        #     lst.append(j['delito']) 
+        # df2 = pd.DataFrame(lst)
+        # df2.set_index("idDelito", inplace=True)
+        # df = df.join(df2, on=["idDelito"], how="left")
+        # df.drop(columns = ["idGrupoDelito", "ordem"], inplace= True)
 
 
-        del df2
+        #del df2
 
         #df.to_csv(f"../data/violencia-mulher-{i}.csv", index=True, encoding="utf-8")
 
-        del df
+        #del df
         print("CSV gerado com sucesso!")
     else:
         print("Requisição 🔴")
 
+
+# %%
