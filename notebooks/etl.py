894c73ce9514266669e26609cd01fc714b899c23
Matheus <matheus.almeida.dev@gmail.com>
2026-08-14 22:30:31 -0300

    feat: inicio de análise

--- notebooks/etl.py
+++ notebooks/etl.py
@@ -7,7 +7,42 @@ for i in range(2011, 2026):
 
     df = pd.concat([df, df2])
 
+del df2
 
-df
+df2 = pd.read_csv("../data/homicidios-mulher-ipea.csv")
 
+df.groupby(["idDelito", "delito"])["total"].sum() #id = 58, 134
+
+dfH = df[(df['idDelito'] == 58) | (df['idDelito'] == 134)]
+
+
+dfH = dfH.groupby(["ano"])[["total"]].sum()
+
+dfH = dfH.reset_index()
+
+dfH["origem"] = "ssp"
+
+df2 = df2.drop(columns=["Região ID"])
+
+df2['origem'] = "ipea"
+
+# %%
+df2.rename(columns={"Período":"ano", "Valor":"total"}, inplace=True)
+dfH = pd.concat([dfH, df2], axis=0)
+dfH.reset_index(inplace=True)
+dfH
+# %%
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.plot(dfH['ano'], dfH['total'], label = "Dados SSP")
+plt.plot(df2["Período"], df2["Valor"], label = "Dados IPEA")
+plt.title("Homícidios de Mulheres no Estado de São Paulo")
+plt.xlabel("Ano")
+plt.ylabel("Número")
+plt.legend()
+plt.show()
+# %%
+dfH
 # %%
--- notebooks/ipea.py
+++ notebooks/ipea.py
@@ -6,5 +6,8 @@ df = pd.read_csv("../data/homicidios-mulheres-estados.csv")
 df["Período"] = pd.to_datetime(df["Período"]).dt.year
 # %%
 
-df[ df["Região ID"] == 35]
+df = df[ df["Região ID"] == 35]
+
+df.to_csv(f"../data/homicidios-mulher-ipea.csv", index=False)
+
 # %%
