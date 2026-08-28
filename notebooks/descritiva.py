c402b386332af1943f34ac748874cfde85b59e88
Matheus Almeida <mathmatheus314@gmail.com>
2026-08-28 19:35:08 -0300

    feat: Tabela de frequência da cor de pele

--- notebooks/descritiva.py
+++ notebooks/descritiva.py
@@ -1,3 +1,16 @@
 # %%
 import pandas as pd
+
+df = pd.read_csv("../data/ssp-feminicidio.csv")
+df.head()
+# %%
+
+tabela = df.groupby("COR_PELE")[["NUM_BO"]].count().sort_values(by="NUM_BO", ascending=False)
+tabela.rename(columns={"NUM_BO": "FrequenciaAbs"}, inplace=True)
+tabela["FrequenciaAbsAcumulada"] = tabela["FrequenciaAbs"].cumsum()
+tabela["FrequenciaRelativa"] = tabela["FrequenciaAbs"]/tabela["FrequenciaAbs"].sum()
+tabela["FrequemcoaRelativaAcumulada"] = tabela["FrequenciaRelativa"].cumsum()
+tabela
+
+
 # %%
